2587e6c8e34fe9be13ab71224167422943dc7367
Kamil Swiatkowski <kamils@juniper.net>
2017-09-11 22:55:33 +0200

    modify k8s cluster name

--- ansible/utils/get_contrail_install_pkg.sh
+++ ansible/utils/get_contrail_install_pkg.sh
@@ -6,17 +6,20 @@
 ######                                                                  ####
 ############################################################################
 #git clone -b R4.0 https://github.com/Juniper/contrail-ansible.git
-echo "Getting Contrail-Ansible Packages............................"
+echo "Getting Contrail-Ansible Packages.........................."
 wget https://s3-us-west-2.amazonaws.com/contrail-docker-images/contrail-ansible.tgz;
-echo "Getting Contrail-Docker-Images............................"
+echo "Getting Contrail-Docker-Images............................."
 wget https://s3-us-west-2.amazonaws.com/contrail-docker-images/contrail-kubernetes-docker-images_4.0.0.0-15.tgz;
 echo "Untar Contrail-Ansible packages............................"
 tar -xvzf contrail-ansible.tgz;
 rm -rf contrail-ansible.tgz;
-echo "Create container_images directory............................"
+echo "Modify k8s cluster name...................................."
+sed -i "s/k8s-default/k8sCluster/g" /root/contrail-ansible/playbooks/roles/node/vars/main.yml
+echo "Create container_images directory.........................."
 mkdir /root/contrail-ansible/playbooks/container_images;
 tar -xvzf /root/contrail-kubernetes-docker-images_4.0.0.0-15.tgz -C /root/contrail-ansible/playbooks/container_images/.;
 #tar -xvzf /root/contrail-ansible/playbooks/container_images/contrail-kubernetes-docker-images_4.0.0.0-11.tgz -C /root/contrail-ansible/playbooks/container_images/.;
 rm -rf contrail-kubernetes-docker-images_4.0.0.0-15.tgz;
 git clone https://github.com/gokulpch/Contrail_Kubernetes_Demo_Yaml.git;
+git clone https://github.com/kswia/kubernetes-demo.git
 echo "Done"
